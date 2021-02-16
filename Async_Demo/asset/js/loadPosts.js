80629758ba95679a682c3ba71c4e49a4df2e40d6
Yonas Assefa <se.yonas.assefa@gmail.com>
2021-02-16 09:59:42 -0800

    bugs were fixed on the loadPosts.js xhr

--- Async_Demo/asset/js/loadPosts.js
+++ Async_Demo/asset/js/loadPosts.js
@@ -8,21 +8,36 @@ function loadPosts() {
     function loadCustomers() {
         const xhr = new XMLHttpRequest();
         //change file URL up on your location
-        xhr.open('GET', './', true);
+        xhr.open('GET', './jsonData/posts.json', true);
         xhr.onload = function() {
         if (this.status === 200) {
         const posts = JSON.parse(this.responseText);
         let output = '';
-        posts.forEach(post => {
-       
-        //same as previous code
-        let output += ` `;
-        }});
+        posts.forEach(postItem => {
+            const post = JSON.parse(postItem.responseText);
+            output = `
+            <div class="item">
+            <div class="image"> <img src="${post.image}"> </div>
+            <div class="content">
+            <a class="header" href="#" id="bTitle"> ${post.postTitle} </a>
+            <div class="meta">
+            <span id="bDate">${post.date} </span>
+            <span>By: <a href="#" id="bAuthor"> ${post.name}</a></span>
+            </div>
+            <div class="description"> <p id="bDesc"> ${post.postText} </p> </div>
+            <div class="extra"> <a class="ui floated basic violet button" href="#">Read Mores</a> </div>
+            </div>
+            </div> `;
+            
+
+        output += ` `;
+
+        })};
        
         postDiv1.innerHTML = output;
         }
-        }
         xhr.send();
-       }
-
+        }
+        
 }
+
